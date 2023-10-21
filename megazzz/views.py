b540cc69f48a7e56f9463975a634516d146afa14
Kreex <marina-denis@ya.ru>
2023-10-21 09:12:50 +0300

    GET- и POST-запросы. Обработчики исключений запросов

---
+++ megazzz/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render
+
+def index(request):
+    return HttpResponse("Страница магазина Мегазон")
+
+def categories(request, cat_id):
+    return HttpResponse(f"<h1>ПРОЦЕССОРЫ</h1><p>id: {cat_id}</p>")
+
+def categories_slug(request, cat_slug):
+    if request.GET:
+        print(request.GET)
+    return HttpResponse(f"<h1>ПРОЦЕССОРЫ</h1><p>slug: {cat_slug}</p>")
+
+def archive(request, year):
+    if year > 2023:
+        raise Http404()
+    return HttpResponse(f"<h1>Поколение процессоров</h1><p>{year}</p>")
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound ('<h1>СТАНИЦА НЕ НАЙДЕНА</h1>')
