7f98478b07b0921bae720df1f9636d0991c06ce0
Kreex <marina-denis@ya.ru>
2023-10-21 12:36:15 +0300

    Перенаправления (redirect). Функция reverse

--- megazzz/urls.py
+++ megazzz/urls.py
@@ -5,8 +5,11 @@ from . import converters
 register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
-    path('', views.index),
-    path('cats/<int:cat_id>/', views.categories),
-    path('cats/<slug:cat_slug>/', views.categories_slug),
-    path("archive/<year4:year>/", views.archive),
+    path('', views.index, name='home'),
+    path('cats/<int:cat_id>/', views.categories, name='cats_id'),
+    path('cats/<slug:cat_slug>/', views.categories_slug, name='cats_s'),
+    path("archive/<year4:year>/", views.archive, name='archive'),
+    path('cats/<slug:a1>/<slug:a2>', views.archive, name='single'),
+    path('cats/music/radiotapok/', views.radiorapok, name='radiotapok'),
+
 ]
--- megazzz/views.py
+++ megazzz/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
 
 def index(request):
     return HttpResponse("Страница магазина Мегазон")
@@ -12,10 +14,21 @@ def categories_slug(request, cat_slug):
         print(request.GET)
     return HttpResponse(f"<h1>ПРОЦЕССОРЫ</h1><p>slug: {cat_slug}</p>")
 
+#def archive(request, year):
+#    if year > 2023:
+#        uri = reverse('cats', args=('sports',))
+#        return redirect(uri)
+#    return HttpResponse(f"<h1>Поколение процессоров</h1><p>{year}</p>")
+
+def radiorapok(request):
+    return HttpResponse(f"<h1>RadioTapok</h1>")
+
 def archive(request, year):
     if year > 2023:
-        raise Http404()
+        uri = reverse('single', kwargs={'a1': 'music', 'a2': 'radiotapok'})
+        return redirect(uri)
     return HttpResponse(f"<h1>Поколение процессоров</h1><p>{year}</p>")
 
 def page_not_found(request, exception):
     return HttpResponseNotFound ('<h1>СТАНИЦА НЕ НАЙДЕНА</h1>')
+
