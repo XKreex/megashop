a07fa3340ff38e3bd208b2eeca0a2551a04cf583
Kreex <marina-denis@ya.ru>
2023-10-26 07:15:08 +0300

    Наследование шаблонов (extends). Тег include

--- megashop/settings.py
+++ megashop/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'megashop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates', # прописываем адрес наших шаблонов
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
