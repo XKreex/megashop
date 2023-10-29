f7f32992214afd4cb129ce9214e498aa174778d6
XKreex <marina-denis@ya.ru>
2023-10-30 00:13:13 +0300

    Создание первой модели Создание и запуск файлов миграций

--- megazzz/models.py
+++ megazzz/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Shop(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    in_stock = models.BooleanField(default=True)
